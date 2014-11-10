50867875172f35dd9c4bc57e511316ea9946ebc5
zhq1943 <engineer1943@163.com>
2014-11-10 23:49:08 +0800

    engine dx
    set up vs2010

---
+++ Engine/Engine/graphicsclass.h
@@ -0,0 +1,29 @@
+#ifndef _GRAPHICSCLASS_H_
+#define _GRAPHICSCLASS_H_
+
+#include <Windows.h>
+
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+private:
+
+};
+
+
+#endif
